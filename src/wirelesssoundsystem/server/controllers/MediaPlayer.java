bb3e7b3efcf59134b27dba06f0292c591ca5525f
eluchsinger <eluchsinger>
2015-12-01 22:59:05 +0100

    Fixed public keyword in Interface MediaPlayer.

--- src/wirelesssoundsystem/server/controllers/MediaPlayer.java
+++ src/wirelesssoundsystem/server/controllers/MediaPlayer.java
@@ -8,32 +8,32 @@ import javafx.beans.property.SimpleObjectProperty;
  * Created by Esteban Luchsinger on 01.12.2015.
  */
 public interface MediaPlayer {
-    public void play();
-    public void pause();
-    public void stop();
-    public boolean isPlaying();
-    public SimpleBooleanProperty getIsPlayingProperty();
+    void play();
+    void pause();
+    void stop();
+    boolean isPlaying();
+    SimpleBooleanProperty getIsPlayingProperty();
 
     /**
      * Toggles playing.
      * If it is playing, it pauses.
      * If it is paused, it plays.
      */
-    default public void togglePlay(){
+    default void togglePlay(){
         if(this.isPlaying())
             this.pause();
         else
             this.play();
     }
 
-    public void nextTrack();
-    public void previousTrack();
+    void nextTrack();
+    void previousTrack();
 
-    public void getCurrentTrack();
-    public SimpleObjectProperty<?> getCurrentTrackProperty();
+    void getCurrentTrack();
+    SimpleObjectProperty<?> getCurrentTrackProperty();
 
-    public void getVolume();
-    public void setVolume();
-    public SimpleDoubleProperty getVolumeProperty();
+    void getVolume();
+    void setVolume();
+    SimpleDoubleProperty getVolumeProperty();
 
 }
