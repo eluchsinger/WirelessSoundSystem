3fdfd9d33e588c26eb86b328b335562e47ea07ef
eluchsinger <eluchsinger>
2015-12-04 11:45:02 +0100

    Created a Singleton that handles the DiscoveryService.

--- src/ch/wirelesssoundsystem/server/controllers/Main.java
+++ src/ch/wirelesssoundsystem/server/controllers/Main.java
@@ -1,5 +1,6 @@
 package ch.wirelesssoundsystem.server.controllers;
 
+import ch.wirelesssoundsystem.server.controllers.networking.discovery.DiscoveryService;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -10,10 +11,16 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+        System.out.println("Starting discovery Service...");
+        DiscoveryService.getInstance().start();
+
         Parent root = FXMLLoader.load(getClass().getResource("../views/MainWindow.fxml"));
         primaryStage.setTitle("Wireless Sound System");
         primaryStage.setScene(new Scene(root, 800, 600));
         primaryStage.show();
+
+        System.out.println("Stopping discovery Service...");
+        DiscoveryService.getInstance().stop();
     }
 
 
--- src/ch/wirelesssoundsystem/server/controllers/networking/discovery/DiscoveryService.java
+++ src/ch/wirelesssoundsystem/server/controllers/networking/discovery/DiscoveryService.java
@@ -1,16 +1,16 @@
 package ch.wirelesssoundsystem.server.controllers.networking.discovery;
 
 import java.net.InetAddress;
-import java.util.Timer;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
 
 /**
- * Created by Esteban Luchsinger on 03.12.2015.
- * This class handles the WSS Discovery Protocol.
+ * Created by Esteban Luchsinger on 04.12.2015.
+ * Singleton handler for the DiscoveryService.
  */
 public class DiscoveryService {
+    private static DiscoveryService ourInstance = new DiscoveryService();
     private InetAddress localAddress;
 
     /**
@@ -22,20 +22,39 @@ public class DiscoveryService {
     // Waiting delay for the task to run again in milliseconds.
     private final long delay = 1000;
 
-    public DiscoveryService(InetAddress localAddress){
-        this.localAddress = localAddress;
-
+    public static DiscoveryService getInstance() {
+        return ourInstance;
     }
 
-    public void start(){
-        this.discoveryScheduledService = Executors.newScheduledThreadPool(1);
+    private DiscoveryService() {
     }
 
+    public void start()
+    {
+        if(this.discoveryScheduledService == null || this.discoveryScheduledService.isShutdown() || this.discoveryScheduledService.isTerminated()) {
+            this.discoveryScheduledService = Executors.newScheduledThreadPool(1);
+
+            this.discoveryScheduledService.scheduleAtFixedRate(new Runnable() {
+                @Override
+                public void run() {
+                    discover();
+                }
+            }, 0, 2, TimeUnit.SECONDS);
+        }
+    }
 
     public void stop(){
+        this.discoveryScheduledService.shutdown();
+
+        this.discoveryScheduledService = null;
     }
 
+    /**
+     * This is the discover method.
+     * This method contains the logic of the discovery protocol.
+     */
     private void discover(){
+        System.out.println("BEEP");
 
     }
 
