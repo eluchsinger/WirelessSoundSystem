a59fb2a7a4a015295921117768d34a92a0fff681
eluchsinger <IEgagzS-105>
2016-05-04 09:50:59 +0200

    Updated SongsHandler to be more careful with the logging.

--- Server/src/main/java/controllers/io/SongsHandler.java
+++ Server/src/main/java/controllers/io/SongsHandler.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 /**
  * <pre>
- * Created by Esteban on 30.11.2015.
+ * Created by Esteban Luchsinger on 30.11.2015.
  * Handles songs (ie: Reading songs from files)
  * </pre>
  */
@@ -19,6 +19,9 @@ public class SongsHandler {
 
     private final Logger logger;
 
+    /**
+     * Default constructor.
+     */
     public SongsHandler() {
         logger = LoggerFactory.getLogger(this.getClass());
     }
@@ -34,24 +37,17 @@ public class SongsHandler {
         // List all files which end with .mp3
         File[] files = f.listFiles((dir, name) -> name.endsWith(".mp3"));
 
-        if(files.length > 0){
-            for(File file : files) {
-                this.logger.info(file.getName());
-            }
-        }
-        else{
+        if(files.length == 0) {
             this.logger.info("No Music Files found.");
         }
 
-        this.logger.info("Reading files...");
-
         List<Song> songList = new ArrayList<>();
-        // Transform all songs into Song.
+        // Transform all songs into Song objects.
         for(File file : files){
             try {
                 Song song = new Mp3Song(file.getPath());
                 songList.add(song);
-                this.logger.info("Song added: " + song.getTitle());
+                this.logger.info("Song added: " + song.getPath());
 
             } catch (Exception e) {
                 this.logger.warn("Fehler beim Laden eines Songs!", e);
